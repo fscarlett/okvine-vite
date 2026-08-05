c029de92e50b3d4e9f9106a82f9c94efec45967e
Fox Scarlett <foxontheone@gmail.com>
2026-08-04 22:21:30 -0700

    color styles, container

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,26 +1,26 @@
 function Footer() {
   return (
-    <footer className='footer relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen border-t border-[#e0e0e0] py-4'>
-      <div className='footer-container w-full max-w-[1400px] mx-auto flex flex-col items-start justify-start gap-4 px-1.5'>
+    <footer className='footer relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen border-t border-[#e0e0e0] py-4 bg-olive-800 text-olive-100'>
+      <div className='footer-container w-full max-w-[1350px] mx-auto flex flex-col items-start justify-start gap-4 px-1.5'>
         <div className='logo text-l font-semibold my-2'>OKVine</div>
         <nav className='my-2'>
           <div className='links flex flex-row items-center justify-start gap-4'>
-            <a href='#' className='text-xs'>
+            <a href='#' className='text-s'>
               Privacy Policy
             </a>
-            <a href='#' className='text-xs'>
+            <a href='#' className='text-s'>
               Terms of Use
             </a>
-            <a href='#' className='text-xs'>
+            <a href='#' className='text-s'>
               Support
             </a>
-            <a href='#' className='text-xs'>
+            <a href='#' className='text-s'>
               Cookie Management
             </a>
           </div>
         </nav>
         <div className='copyright text-xs my-2'>
-          &copy; 2026 OKVine. All rights reserved.
+          &copy; 2026 OKVine Holdings LLC. All rights reserved.
         </div>
       </div>
     </footer>
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <>
-      <div className='topbar relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen border-b border-[#e0e0e0] h-[50px]'>
-        <div className='h-full w-full mx-auto flex items-center justify-center'>
-          <div className='topbar-container flex flex-row justify-between items-center h-full w-full max-w-[1400px] py-0.5 px-1.5'>
+      <div className='topbar relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen border-b border-[#e0e0e0] h-[50px] --color-olive-800'>
+        <div className='h-full w-full mx-auto flex items-center justify-center bg-olive-800 text-olive-100'>
+          <div className='topbar-container flex flex-row justify-between items-center h-full w-full max-w-[1350px] py-0.5 px-1.5'>
             <div className='logo flex items-center'>
               <a href='/'>
                 <span className='font-semibold text-xl'>OKVine</span>
