1782dc2fd03aeebf76337c81cdb3a37b51ed6beb
Fox Scarlett <foxontheone@gmail.com>
2026-08-04 17:15:39 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>OK Vine</h1>
+        <h2>Food and Entertainment for Vinedale </h2>
+      </section>
+    </>
+  )
+}
+
+export default App
